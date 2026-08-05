6743692ba24fe113e24b5cdb87aa2d2e6ca9efc3
snmaccio-dev <smaccio@frba.utn.edu.ar>
2026-08-05 18:22:34 -0300

    Corrigiendo los errores

--- src/test/java/gestion/GestorDonaciones.java
+++ src/test/java/gestion/GestorDonaciones.java
@@ -1,6 +1,8 @@
 package gestion;
 
 import donatrack.model.donacion.Donacion;
+import donatrack.notificacion.Notificador;
+import donatrack.notificacion.NotificadorWhatsApp;
 import donatrack.repositorio.RepositorioDonaciones;
 
 import java.util.List;
@@ -8,7 +10,7 @@ import java.util.List;
 public class GestorDonaciones {
 
     private final SegmentadorDonaciones segmentacion = new SegmentadorDonaciones();
-    private final GestorPersonas.ServicioNotificaciones servicioNotificaciones = new GestorPersonas.ServicioNotificaciones();
+    private final Notificador notificador = new NotificadorWhatsApp();
     private final RepositorioDonaciones repositorio = RepositorioDonaciones.getInstance();
 
     public List<Donacion> todas() {
