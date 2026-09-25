d7815de4d9dc6bd403e62f6851101734b6c78572
HernanMuniz1 <hmuniz@frba.utn.edu.ar>
2026-09-25 17:31:21 -0300

    persistencia terminada

--- donaciones/src/test/java/donatrack/donaciones/domain/donacion/estado/EstadoDonacionTest.java
+++ donaciones/src/test/java/donatrack/donaciones/domain/donacion/estado/EstadoDonacionTest.java
@@ -6,7 +6,6 @@ import donatrack.donaciones.domain.donacion.Bien;
 import donatrack.donaciones.domain.donacion.CondicionBien;
 import donatrack.donaciones.domain.donacion.Donacion;
 import donatrack.donaciones.domain.catalogo.Unidades;
-import donatrack.logistica.domain.flota.Camion;
 import donatrack.donaciones.domain.persona.Beneficiaria;
 import donatrack.donaciones.domain.persona.Donante;
 import donatrack.donaciones.domain.persona.Genero;
@@ -22,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class EstadoDonacionTest {
 
   @Test
-  public void recorridoFelizAtraviesaLos7EstadosHastaEntregada() {
+  public void recorridoFelizLlegaHastaEntregada() {
     Beneficiaria escuela = beneficiaria();
     Donacion donacion = donacionDeCampera();
     assertEquals("EN_DEPOSITO", donacion.getEstado().getNombre());
@@ -30,7 +29,6 @@ public class EstadoDonacionTest {
     donacion.confirmarDestino(escuela);
     assertEquals("ASIGNACION_REALIZADA", donacion.getEstado().getNombre());
 
-    donacion.asignarCamion(unCamion());
     donacion.marcarListaParaEntregar();
     assertEquals("LISTA_PARA_ENTREGAR", donacion.getEstado().getNombre());
 
@@ -58,7 +56,7 @@ public class EstadoDonacionTest {
   }
 
   @Test
-  public void marcarEnDepositoLimpiaDestinatarioYCamion() {
+  public void marcarEnDepositoLimpiaDestinatarioYDatosDeEntrega() {
     Beneficiaria escuela = beneficiaria();
     Donacion donacion = donacionEnTraslado(escuela);
     donacion.marcarEntregaFallida("Motivo");
@@ -66,7 +64,7 @@ public class EstadoDonacionTest {
     donacion.marcarEnDeposito();
     assertEquals("EN_DEPOSITO", donacion.getEstado().getNombre());
     assertNull(donacion.getDestinatarioAsignado());
-    assertNull(donacion.getCamion());
+    assertNull(donacion.getDatosEntrega());
   }
 
   @Test
@@ -107,7 +105,6 @@ public class EstadoDonacionTest {
   private Donacion donacionEnTraslado(Beneficiaria escuela) {
     Donacion donacion = donacionDeCampera();
     donacion.confirmarDestino(escuela);
-    donacion.asignarCamion(unCamion());
     donacion.marcarListaParaEntregar();
     donacion.marcarEnTraslado();
     return donacion;
@@ -116,9 +113,5 @@ public class EstadoDonacionTest {
   private Beneficiaria beneficiaria() {
     PersonaJuridica org = new PersonaJuridica("30-11111111-1", "Escuela Test", TipoOrganizacion.INSTITUCION, "Educacion");
     return new Beneficiaria(org);
-  }
-
-  private Camion unCamion() {
-    return new Camion("AAA111", 10, 3, 1000);
   }
 }
